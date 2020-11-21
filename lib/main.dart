d41c67b5fff5ac79805c5b0d02f2717d80d3a5b2
Nav Singh <navsingh191@gmail.com>
2020-11-20 23:40:59 -0500

    micard image added and name text widget added

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,29 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 200.0,
+                ),
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/me.jpeg'),
+                ),
+                Text(
+                  "Nav Singh",
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     ),
   );
