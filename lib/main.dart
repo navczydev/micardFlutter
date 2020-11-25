869c25cf98102a681b50852b0aa4f85072a8c88c
Nav Singh <navsingh191@gmail.com>
2020-11-24 22:33:52 -0500

    card chapter finish

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,12 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Center(
+        body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(32.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  height: 200.0,
-                ),
                 CircleAvatar(
                   radius: 50.0,
                   backgroundImage: AssetImage('images/me.jpeg'),
@@ -34,7 +32,58 @@ void main() {
                       letterSpacing: 2.5,
                       fontWeight: FontWeight.bold,
                       color: Colors.teal.shade100),
-                )
+                ),
+                SizedBox(
+                  height: 24.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  elevation: 8.0,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+                  color: Colors.white,
+                  child: Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.call,
+                          size: 32.0,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          "5*4-***-1**3",
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20.0),
+                        ),
+                      )),
+                ),
+                Card(
+                  elevation: 8.0,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                  color: Colors.white,
+                  child: Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.email,
+                          size: 32.0,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          "n*******1**@*****.com",
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20.0),
+                        ),
+                      )),
+                ),
               ],
             ),
           ),
